fa85889158b2ce6fe847eaf40b9bcf91ac996af0
STM32G0 <stm32g0@gmail.com>
2023-03-15 17:49:12 +0100

    init

---
+++ STM32G070_BareMetalSzablon/Src/main.c
@@ -0,0 +1,25 @@
+/*****************************************
+File  : main.c
+Autor : strefapic.blogspot.com
+MCU   : STM32G070RBT6
+IDE   : STM32Cube IDE
+REV   : x.x
+******************************************/
+
+#include <stm32g070xx.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "stm32g0xx_it.h" // interrupt handlers
+
+
+
+
+int main(void) {
+
+
+	while(1){
+
+
+	}
+}
